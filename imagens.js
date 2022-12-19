19f9b8c99b06b8b9d4c6cd78beff1e3c5791adfe
Felipe da Silva <felipedasilvabispodossantos@gmail.com>
2022-12-18 22:29:51 -0300

    Adição de 2 carros e organização do código em pastas

--- imagens.js
+++ imagens.js
@@ -1,8 +1,29 @@
+//imagens atores
+let estrada;
+let vaca;
+let carro1;
+let carro2;
+let carro3;
+let carro4;
+let carro5;
+
+//posiçoes atores
+let xCarro1 = 600;
+let xCarro2 = 400;
+let xCarro3 = 150;
+let xCarro4 = 0;
+let xCarro5 = 227;
+let yVaca = 368;
+
+let pontuacao = 0;
+
 function preload(){
   estrada = loadImage("imagens/estrada.png");
   vaca = loadImage("imagens/ator-1.png");
   carro1 = loadImage("imagens/carro-1.png");
   carro2 = loadImage("imagens/carro-2.png");
   carro3 = loadImage("imagens/carro-3.png");
+  carro4 = loadImage("imagens/carro-2.png");
+  carro5 = loadImage("imagens/carro-1.png");
 }
 
